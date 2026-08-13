4e1af7182e72f6d662b21f4d2a4657c861fe217b
Mauricio García <maugarciag17@gmail.com>
2026-08-13 06:52:21 -0500

    Initial commit

---
+++ src/main/java/com/yam/yam_funds_api/YamFundsApiApplication.java
@@ -0,0 +1,13 @@
+package com.yam.yam_funds_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamFundsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamFundsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yam/yam_funds_api/YamFundsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yam.yam_funds_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamFundsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
